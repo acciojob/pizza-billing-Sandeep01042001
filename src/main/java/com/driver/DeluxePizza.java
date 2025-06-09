d09f880ed9409519d81619abf051aa3fe9811b9c
Sandeep01042001 <sandeep7668111757@gmail.com>
2025-06-09 17:44:41 +0530

    Update code

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,6 +5,6 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         addExtraCheese();
-        addTakeaway();  
+        addTakeaway();
     }
 }
